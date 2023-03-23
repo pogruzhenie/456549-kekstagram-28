40552088a4ae890a1dff3f7744f635087903b214
pogruzhenie <pogruzhenie@gmail.com>
2023-03-23 20:48:29 +0300

    Генерация уникального идентификатора из диапазона

--- js/main.js
+++ js/main.js
@@ -74,6 +74,23 @@ const createIdGenerator = () => {
 const generatePhotoId = createIdGenerator();
 const generateCommentId = createIdGenerator();
 
+function createRandomIdFromRangeGenerator (min, max) {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      console.error('Перебраны все числа из диапазона от ' + min + ' до ' + max);
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+
 const generateComment = () => {
 
   return {
