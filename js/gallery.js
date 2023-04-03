f7ffec338dcd770a45b585ef07a7cf4268ac2676
pogruzhenie <pogruzhenie@gmail.com>
2023-04-03 20:38:34 +0300

    получаем путь, описание, лайки комментарии

--- js/gallery.js
+++ js/gallery.js
@@ -1,24 +1,63 @@
+import { generatePhotos } from './data.js';
+import { addPictures } from './pictures.js';
+
+const randomPhotos = generatePhotos();
+
+addPictures(randomPhotos);
+console.log(randomPhotos);
+
 const gallery = () => {
   const picturesContainer = document.querySelector('.pictures');
-  const bigPicture = document.querySelector('.big-picture');
+  const photoDetail = document.querySelector('.big-picture');
+  const closeButton = photoDetail.querySelector('.big-picture__cancel');
+  const photoImg = photoDetail.querySelector('.big-picture__img').querySelector('img');
+  const likesCount = photoDetail.querySelector('.likes-count');
+  const commentsCount = photoDetail.querySelector('.comments-count');
+  const photoCaption = photoDetail.querySelector('.social__caption');
+
   console.log('gallery module connected');
   console.log(picturesContainer);
 
   const modalClose = (evt) => {
     evt.preventDefault();
-    bigPicture.classList.add('hidden');
+    photoDetail.classList.add('hidden');
     document.body.classList.remove('modal-open');
   };
 
   const onPictureClick = (evt) => {
     evt.preventDefault();
-    console.log(evt.target);
-    bigPicture.classList.remove('hidden');
+
+    let photoObject = randomPhotos[evt.target.dataset.id - 1];
+    let photoComments = photoObject.comments;
+    const commentsListFragment = document.createDocumentFragment();
+
+    photoDetail.classList.remove('hidden');
     document.body.classList.add('modal-open');
-    const closeButton = document.querySelector('.big-picture__cancel');
+
+    photoImg.src = evt.target.src;
+    photoImg.alt = photoObject.description;
+    likesCount.textContent = photoObject.likes;
+    commentsCount.textContent = photoComments.length;
+    photoCaption.textContent = photoObject.description;
+
+    photoComments.forEach((comment) => {
+      console.log(comment);
+    });
+
+    console.log(evt);
+    console.log(evt.target);
+    console.log(photoImg.src);
+    console.log(evt.target.src);
+    console.log('photo id ' + evt.target.dataset.id);
+    console.log(randomPhotos[evt.target.dataset.id - 1]);
+    console.log(photoComments);
+
+    //pictureId = evt.target.dataset.id;
+
     closeButton.addEventListener('click', modalClose);
   };
 
   picturesContainer.addEventListener('click', onPictureClick);
 };
+
 export { gallery };
