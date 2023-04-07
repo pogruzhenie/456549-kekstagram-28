d7e1a8fb060edb4484c8344356f33c2b1ac94261
pogruzhenie <pogruzhenie@gmail.com>
2023-04-07 04:23:42 +0300

    октрытие формы при загрузке файла в input

--- js/form.js
+++ js/form.js
@@ -1,1 +1,19 @@
+import { manageModal } from './popup.js';
 console.log('form module connected');
+
+const photoUploadControl = document.querySelector('#upload-file');
+
+const onPhotoUpload = (evt) => {
+  console.log(evt.target);
+  console.log('input changed');
+
+  const imgUploadOverlay = document.querySelector('.img-upload__overlay');
+  const closeButton = document.querySelector('#upload-cancel');
+
+  //imgUploadOverlay.classList.remove('hidden');
+  //document.body.classList('modal-open');
+  manageModal(imgUploadOverlay, closeButton);
+  //closeButton
+};
+
+photoUploadControl.addEventListener('change', onPhotoUpload);
