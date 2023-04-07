705cb8602e28c6c1f9428c0ab59c394e5bc038c5
pogruzhenie <pogruzhenie@gmail.com>
2023-04-07 04:24:52 +0300

    вспомнили точку с запятой

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import { generatePhotos } from './data.js';
 import { addPictures } from './thumbnails.js';
-import { showDetail } from './detail.js'
+import { showDetail } from './detail.js';
 import './form.js';
 
 const randomPhotos = generatePhotos();
